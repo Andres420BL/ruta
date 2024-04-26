11b8e611046b45cc57165d648127e7b9627dfa72
Jose <andres.begambrela@amigo.edu.co>
2024-04-26 10:53:42 -0500

    actualizacion 26-04-2024_1053
    menu principal
    rutas
    funcion agregar nuevas rutas

--- program.py
+++ program.py
@@ -15,5 +15,11 @@ def fnt_menu():
     r = cod + ' ' + descripcion
     list_rutas.append(r)
     input('\nRuta registrada con exito <enter>')
-
+def fnt_menu():
+    fnt_limpiar()
+    opcion_r= input('<< Menu de rutas >>\n1.Agregar\n2.Consultar\n3.Salir\n->')
+    if opcion_r == '1':
+        fnt_agregar_ruta()
+    if opcion_r=='3':
+        sw2= False
 
